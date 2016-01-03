62d6bf05771011f670994c3ce47149842a846eaf
seregaNV <serega.n.v333@gmail.com>
2016-01-03 19:26:07 +0200

    Добавив возможность удаляти ітеми.

--- js/exampleFactory.js
+++ js/exampleFactory.js
@@ -1,10 +1,11 @@
 (function() {
     "use strict";
-    var app = angular.module('app');
-    app.factory('exampleFactory', function() {
+    function eF() {
         console.log('example factory');
         return {
             arr: ['HTML', 'CSS', 'JavaScript', 'jQuery', 'Bootstrap', 'NodeJS', 'ExpressJS']
         };
-    });
+    }
+    var app = angular.module('app');
+    app.factory('exampleFactory', eF);
 })();
--- js/main.controller.js
+++ js/main.controller.js
@@ -1,12 +1,34 @@
 (function() {
     function MainController($scope, eF){
-        $scope.name = '';
         $scope.show = false;
         $scope.data = eF.arr;
         $scope.click = function() {
-            $scope.data.push($scope.name);
-            console.log(eF.arr);
+            var len = $scope.data.length,
+                check = true;
+            for (var i = 0; i < len; i++) {
+                if ($scope.name === $scope.data[i]) {
+                    check = false;
+                }
+            }
+            if (check && $scope.name) {
+                $scope.data.push($scope.name);
+                console.log('"' + $scope.name + '" is added.');
+            } else {
+                console.log('Item is already exists!');
+            }
+            console.log('arr: ', $scope.data);
+        };
+        $scope.remove = function(item) {
+            console.log('"' + item + '" is removed.');
+            var len = $scope.data.length;
+            for (var i = 0; i < len; i++) {
+                if (item === $scope.data[i]) {
+                    $scope.data.splice(i, 1);
+                }
+            }
+            console.log('arr: ', $scope.data);
         };
     }
+
     angular.module('app').controller('MainController', ["$scope", 'exampleFactory', MainController]);
 })();
